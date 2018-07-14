9f1201bb6f74f5211e0fb7f96ab0b7004330487e
shutsugan <shutsugan@gmail.com>
2018-07-14 20:20:14 +0100

    create the structure and wrote the gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const gulp = require('gulp');
+const minifyCss = require('gulp-csso');
+const concat = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
+
+gulp.task('css', _ => {
+	return gulp.src('src/**/*.css')
+		.pipe(minifyCss())
+		.pipe(gulp.dest('build'));
+});
+
+gulp.task('js', _ => {
+	return gulp.src('src/**/*.js')
+		.pipe(sourcemaps.init())
+		.pipe(babel({presets: ['env']}))
+		.pipe(concat('min.js'))
+		.pipe(uglify())
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('build/js'));
+});
+
+gulp.task('default', ['css', 'js']);
---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+//select all the element with title attribute of data-tooltip
+//when hovering a selected element
+	//create a div in the body
+	//fill the div with the element title's text
+	//position the tooltip above the element
+	//add a class to animate the appearance
+
+//when hoverout 
+	//remove the class for animating the removal
+	//in end transition remove the div from the body
